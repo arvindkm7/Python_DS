6c51414acef4f77eb9c5f27fe28dd495e85a86e2
arvindkm7 <arvindkumar771377@gmail.com>
2026-07-13 06:47:10 +0530

    DSA

--- 6_Largest_continuos_sub_arr.py
+++ 6_Largest_continuos_sub_arr.py
@@ -1,5 +1,36 @@
 def sum_sub_arr(arr):
+    large_sum, cur_sum, i, j, k = 0, 0, 0, 1, 2
+    large_lst = []
     
-    return(lst[])
+    for i in range(0, len(arr) - 3):
+        cur_sum = arr[i] + arr[j] + arr[k]
+
+        if cur_sum >= large_sum:
+            large_sum = cur_sum
+            if len(large_lst) == 0:
+                large_lst.append(arr[i])
+                large_lst.append(arr[j])
+                large_lst.append(arr[k])
+            else:
+                large_lst.clear()
+                large_lst.append(arr[i])
+                large_lst.append(arr[j])
+                large_lst.append(arr[k])
+        j += 1
+        k += 1
+        # print(f'large_lst : {large_lst}, large_sum :{large_sum }')
+    return large_lst
+        
+def kadane(arr):
+    max_sum = arr[0]
+    current_sum = arr[0]
+
+    for i in range(1, len(arr)):
+        current_sum = max(arr[i], current_sum + arr[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
 arr = [-1, 5, 6, -5, -6, 5, -8, 5, 6, 7, -5]
 print(sum_sub_arr(arr))
+print(kadane(arr))
